085d268ddcb02c821212ad84e90cfe3511897e00
Dmitry Lugovoy <dmit.lugovoy@gmail.com>
2022-07-15 22:30:43 +0200

    Add background audio

---
+++ 3dui-wayfinding-unity-project/Assets/Features/FirstPersonController/Effects/CorrectnessAudio.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine.Serialization;
+
+public class CorrectnessAudio : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject characterSignalsInterfaceTarget;
+    [SerializeField]
+    private HologramPath _hologramPath;
+    private ICharacterSignals _characterSignals;
+    
+    [SerializeField] private AudioSource _audioSource;
+    
+    private float _intensityChangeSpeed = 0.1f;
+
+    private float _startPitch = 2f;
+
+    private float _lowPitch = 0.5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterSignals.Moved.Subscribe(w =>
+        {
+            float scaledCorrectness = (_hologramPath.CorrectnessScale.Value * (_startPitch - _lowPitch)) + _lowPitch;
+            _audioSource.pitch = Mathf.Lerp(_audioSource.pitch, scaledCorrectness,
+                w.magnitude * _intensityChangeSpeed);
+        }).AddTo(this);
+    }
+
+    void Awake()
+    {
+        _audioSource.pitch = _startPitch;
+        _characterSignals =
+            characterSignalsInterfaceTarget.GetComponent<ICharacterSignals>();
+    }
+}
